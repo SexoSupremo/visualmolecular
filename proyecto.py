38bf0b94bb211988ea4e5d725c6ad7865bd9ab47
Esteban Brizuela <ehequiel911@gmail.com>
2023-12-18 12:40:44 -0300

    Se agrego la estructura de la proteina desde un archivo pdb

--- proyecto.py
+++ proyecto.py
@@ -2,3 +2,7 @@ from Bio.PDB import PDBParser
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+
+# Cargar la estructura de la proteína desde un archivo PDB
+parser = PDBParser()
+structure = parser.get_structure('mi_proteina', 'C:\\DATABASE\\Universidad Comunera\\Programación 2\\Programación\\messi\\8f48.pdb')
